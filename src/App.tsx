76c2d4c82464867c49923a8b205f6e1978e99cb0
floatingmess06 <amandashore2468@gmail.com>
2024-02-25 13:02:02 +0530

    Enhanced graph functionality to dynamically adjust based on data changes.

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  showGraph: boolean,
 }
 
 /**
@@ -20,8 +21,10 @@ class App extends Component<{}, IState> {
 
     this.state = {
       // data saves the server responds.
+      // showGraph represents condition for showing graph
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      showGraph: false,
     };
   }
 
@@ -29,18 +32,33 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+    if (this.state.showGraph){
+      return (<Graph data={this.state.data}/>)
+    }
   }
 
   /**
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
+    // x : a var to limit no of total requests
+    let x = 0;
+    const interval = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        // Update the state by assigning data in the state to the new data from server
+        // and making the graph visible
+        this.setState({ 
+          data: serverResponds,
+          showGraph:true 
+        });
+      });
+      x++;
+      if (x > 1000){
+        // to stop requesting after 1000 requests
+        clearInterval(interval);
+      }
+    },100)
+    
   }
 
   /**
